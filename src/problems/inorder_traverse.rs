87488be2cc711eb64d2e267b21fdfcec58be90a2
victorshinkevich <supostatekx@gmail.com>
2023-01-04 14:56:01 +0300

    add iterative inorder traversal

--- src/problems/inorder_traverse.rs
+++ src/problems/inorder_traverse.rs
@@ -23,11 +23,11 @@ struct Solution;
 
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-        brute_force(root)
+        recursive(root)
     }
 }
 
-fn brute_force(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+fn recursive(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     let mut result = vec![];
 
     fn traversal(node: Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
@@ -46,12 +46,37 @@ fn brute_force(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     result
 }
 
+fn iterative(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut current_node = root;
+    let mut stack = Vec::<Rc<RefCell<TreeNode>>>::new();
+    let mut result = vec![];
+
+    loop {
+        match (&current_node, &mut stack) {
+            (Some(node), stack) => {
+                stack.push(node.clone());
+                let left_node = node.borrow_mut().left.take();
+                current_node = left_node;
+            }
+            (None, stack) if !stack.is_empty() => {
+                let node = stack.pop().unwrap();
+                let mut node_ref = node.borrow_mut();
+                result.push(node_ref.val);
+                current_node = node_ref.right.take();
+            }
+            (None, _) => break,
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[cfg(test)]
-    mod brute_force {
+    mod iterative {
         use super::*;
 
         #[test]
@@ -70,7 +95,31 @@ mod tests {
                 }))),
             })));
 
-            assert_eq!(brute_force(root), vec![1, 3, 2])
+            assert_eq!(iterative(root), vec![1, 3, 2])
+        }
+    }
+
+    #[cfg(test)]
+    mod recursive {
+        use super::*;
+
+        #[test]
+        fn example_1() {
+            let root = Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 3,
+                        left: None,
+                        right: None,
+                    }))),
+                    right: None,
+                }))),
+            })));
+
+            assert_eq!(recursive(root), vec![1, 3, 2])
         }
     }
 }
